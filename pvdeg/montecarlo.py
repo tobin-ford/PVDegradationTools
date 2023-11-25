854abdc601349c62e54d7ab49700258a8adeac1c
tobin-ford <tford@nrel.gov>
2023-11-24 19:28:45 -0700

    generate correlated data

--- pvdeg/montecarlo.py
+++ pvdeg/montecarlo.py
@@ -11,15 +11,22 @@ from scipy import stats
 from . import spectral
 from . import temperature
 
-# Quasi-Monte Carlo could be used to generate samples matrix
-# best way of doing this might be to have a function for each of the equations in the degredation database, we dont need every solution
-
 class ArugmentError(Exception):
     pass
 
+
 def _symettric_correlation_matrix(*args):
+    """
+    Helper function. creates a symettric correlation matrix from correlation coefficients
+
+    Parameters
+    ----------
+    ### replace this but I didn't know how to write it better
+    ### takes correlation coefficients as arguments = float
+    ### ex : corr_X_Y = float
+    """
+
     valid_corr = ['corr_Ea_X', 'corr_Ea_LnR0', 'corr_X_LnR0']
-    
     for arg in args:
         if arg not in valid_corr:
             raise ArugmentError(f"invalid argument: {arg}. valid arguments are {valid_corr}") 
@@ -50,9 +57,52 @@ def _symettric_correlation_matrix(*args):
     return symmetric_matrix
 
 
+# this probably isnt supposed to be *args
+# it should probably just be the dictionary as shown in correlated()
+def correlated_data_matrix(iterations, *args):
+    """
+    Helper function. Generates a matrix of samples for all modeling constants
+    
+    ----------
+    iterations : integer
+        number of samples to run for monte carlo simulation
+    modeling_constants : dict, key : string, value : list[float]
+        contains appropriate modeling constants and their respective mean and standard deviation
+        ### I think *args should just become the dictionary
+
+    Returns
+    ----------
+    correlated_df : pd.DataFrame
+        returns a tall DataFrame of correlated data to be used in monte carlo trials with appropriate columns named as modeling constants 
+    """
+    valid_modeling_constants = ['R_0', 'lnR_0', 'R_D', 'E_a', 't_fail', 'A', 'FF', 'B', 'E'] # stopped at LeTID 'v_ab'
+
+    for arg in args:
+        if arg not in valid_modeling_constants:
+            raise ArugmentError(f"invalid argument: {arg}. valid arguments are {valid_modeling_constants}") 
+        if (arg >= -1 and arg <= 1):
+            raise ValueError(f"invalid value: {arg}. valid arguments include [-1, 1]")
+
+    uncorrelated_samples_matrix = np.random.normal(loc=0, scale=1, size=(len(args), iterations))
+    coefficient_matrix = _symettric_correlation_matrix(args)
+    correlated_samples = np.matmul(coefficient_matrix, uncorrelated_samples_matrix)
+    correlated_samples = np.matrix(correlated_samples)
+
+    keys_list = list(args.keys())
+    mean_list = [value[0] for value in args.values()]
+    stdev_list = [value[1] for value in args.values()]
+
+    stdev_matrix = np.transpose(np.matrix(stdev_list))
+    result = np.multiply(correlated_samples, stdev_matrix) + np.transpose(np.matrix([mean_list]))
+
+    correlated_df = pd.DataFrame(np.transpose(result), columns=keys_list)
+    return correlated_df
+
+
+
 def correlated(weather_df, meta,
                function,
-               MC_params
+               MC_params, samples, corr
                ):
     """
     Parameters
@@ -66,3 +116,7 @@ def correlated(weather_df, meta,
         value of list containing appropritate mean, standard deviation as floats
         could look like  {'ea' : [mean, stdev] }
     """
+
+    A = _symettric_correlation_matrix(corr)
+    decomposition = cholesky(A, lower = True)
+
